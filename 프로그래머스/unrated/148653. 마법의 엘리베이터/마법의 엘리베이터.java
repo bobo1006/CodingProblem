a12d8a5ee34eb00a9d3cfe613a26c3ff944cf820
bosung <105060079+bobo1006@users.noreply.github.com>
2023-07-24 17:04:18 +0900

    [unrated] Title: 마법의 엘리베이터, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int solution(int storey) {
+        int answer = 0;
+
+        while(storey > 0){
+            int temp = storey % 10;
+            storey /= 10;
+
+            if (temp < 5){
+                answer += temp;
+            }
+            else if (temp > 5){
+                answer += 10 - temp;
+                storey ++;
+            }
+            else {
+                if (storey % 10 < 5){
+                    answer += temp;
+                }
+                else {
+                    answer += temp;
+                    storey++;
+                }
+            }
+
+
+        }
+        return answer;
+    }
+}
