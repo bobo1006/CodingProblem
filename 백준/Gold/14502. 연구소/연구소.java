4d9e713ef55b6b14e500c9d66bf19aa6264fc20c
bosung <105060079+bobo1006@users.noreply.github.com>
2024-03-10 20:02:55 +0900

    [Gold IV] Title: 연구소, Time: 964 ms, Memory: 299376 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -0,0 +1,85 @@
+import java.io.*;
+import java.util.*;
+class Main {
+    static int N,M;
+    static int[] dx = {1,-1,0,0};
+    static int[] dy = {0,0,-1,1};
+    static int max = 0;
+    static ArrayList<int[]> virus;
+    static int[][] map;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        boolean[][] wall = new boolean[N][M];
+        virus = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 2) virus.add(new int[]{i,j});
+            }
+        }
+        dfs(0,wall);
+        System.out.println(max);
+    }
+    public static void dfs(int cntWall, boolean[][] wall){
+        if (cntWall == 3){
+            max = Math.max(getSafeZone(), max);
+            return;
+        }
+
+        for (int i=0;i<N;i++){
+            for (int j=0;j<M;j++){
+                if (map[i][j] == 0 && !wall[i][j]){
+                    map[i][j] = 1;
+                    wall[i][j] = true;
+                    dfs(cntWall + 1, wall);
+                    map[i][j] = 0;
+                    wall[i][j] = false;
+                }
+            }
+        }
+    }
+    public static int getSafeZone(){
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[N][M];
+        for (int[] tmp:virus){
+            queue.add(tmp);
+            visited[tmp[0]][tmp[1]] = true;
+        }
+        int[][] mapClone = new int[N][M];
+        for (int i=0;i<N;i++){
+            mapClone[i] = map[i].clone();
+        }
+
+        while (!queue.isEmpty()){
+            int[] now = queue.poll();
+
+            for (int i=0;i<4;i++){
+                int nextX = now[0] + dx[i];
+                int nextY = now[1] + dy[i];
+
+                if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) continue;
+                if (mapClone[nextX][nextY] == 0 && !visited[nextX][nextY]){
+                    visited[nextX][nextY] = true;
+                    mapClone[nextX][nextY] = 2;
+                    queue.add(new int[]{nextX, nextY});
+                }
+            }
+        }
+        int cnt = 0;
+
+        for (int i=0;i<N;i++){
+            for (int j=0;j<M;j++){
+                if (mapClone[i][j] == 0){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+}
