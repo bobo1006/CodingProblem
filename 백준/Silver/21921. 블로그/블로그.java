8f73d466b75a1ded92bbdb1ed13f4473ea493d6a
bosung <105060079+bobo1006@users.noreply.github.com>
2024-03-17 15:33:20 +0900

    [Silver III] Title: 블로그, Time: 360 ms, Memory: 35180 KB -BaekjoonHub

---
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i=0;i<N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        }
+        int cnt = 1;
+        int sum = 0;
+
+        for (int i=0;i<X;i++){
+            sum += arr[i];
+        }
+        int max = sum;
+
+        for (int i=X;i<N;i++){
+            sum -= arr[i-X];
+            sum += arr[i];
+            if (sum == max) cnt++;
+            if (sum > max){
+                cnt = 1;
+                max = sum;
+            }
+        }
+        if (max == 0) System.out.println("SAD");
+        else{
+            System.out.println(max);
+            System.out.println(cnt);
+        }
+    }
+}
