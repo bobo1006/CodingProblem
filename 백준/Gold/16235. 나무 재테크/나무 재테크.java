d99a7beb3947878ecfd3152c98ccee8449138124
bosung <105060079+bobo1006@users.noreply.github.com>
2024-04-12 18:44:00 +0900

    [Gold III] Title: 나무 재테크, Time: 608 ms, Memory: 20544 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.java
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.java
@@ -4,20 +4,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static class Tree implements Comparable<Tree>{
-        int x,y;
-        int age;
-        Tree(int x, int y, int age){
-            this.x = x;
-            this.y = y;
-            this.age = age;
-        }
-        @Override
-        public int compareTo(Tree o){
-            return this.age - o.age;
-        }
-    }
-    static PriorityQueue<Tree> trees1 = new PriorityQueue<>();
+    static ArrayDeque<Integer>[][] trees;
     static int[][] map, A;
     static int N;
     static int[][] dir = {{-1,-1}, {-1,0}, {-1, 1}, {0,-1}, {0,1}, {1,-1}, {1,0}, {1,1}};
@@ -28,7 +15,8 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-
+        
+        trees = new ArrayDeque[N][N];
         map = new int[N][N];
         for (int i=0;i<N;i++){
             Arrays.fill(map[i], 5);
@@ -39,6 +27,7 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             for (int j=0;j<N;j++){
                 A[i][j] = Integer.parseInt(st.nextToken());
+                trees[i][j] = new ArrayDeque();
             }
         }
         for (int i=0;i<M;i++){
@@ -46,54 +35,62 @@ public class Main {
             int x = Integer.parseInt(st.nextToken()) - 1;
             int y = Integer.parseInt(st.nextToken()) - 1;
             int age = Integer.parseInt(st.nextToken());
-            trees1.add(new Tree(x,y,age));
+            trees[x][y].add(age);
         }
-        Queue<Tree> dead = new LinkedList<>();
-        Queue<Tree> breeding = new LinkedList<>();
 
         for (int i=0;i<K;i++){
-            trees1 = spring(dead, breeding);
-            summer(dead);
-            autumn(breeding);
+            springAndSummer();
+            fall();
             winter();
         }
-        System.out.println(trees1.size());
-    }
-    private static PriorityQueue<Tree> spring(Queue<Tree> dead, Queue<Tree> breeding){
-        PriorityQueue<Tree> trees2 = new PriorityQueue<>();
-
-        while (!trees1.isEmpty()){
-            Tree now = trees1.poll();
-            if (now.age > map[now.x][now.y]){
-                dead.add(now);
+        int total = 0;
+        for (int i=0;i<N;i++){
+            for (int j=0;j<N;j++){
+                total += trees[i][j].size();
             }
-            else{
-                map[now.x][now.y] -= now.age;
-                Tree tree = new Tree(now.x, now.y, now.age+1);
-                if (tree.age % 5 == 0){
-                    breeding.add(tree);
+        }
+        System.out.println(total);
+    }
+    private static void springAndSummer(){
+        for (int i=0;i<N;i++){
+            for (int j=0;j<N;j++){
+                int size = trees[i][j].size();
+                int nutrient = 0;
+
+                for (int k=0;k<size;k++){
+                    int now = trees[i][j].pollFirst();
+                    if (map[i][j] < now){
+                        nutrient += now/2;
+                    }
+                    else{
+                        map[i][j] -= now;
+                        trees[i][j].add(now + 1);
+                    }
                 }
-                trees2.add(tree);
+                map[i][j] += nutrient;
             }
         }
 
-        return trees2;
     }
-    private static void summer(Queue<Tree> dead){
-        while (!dead.isEmpty()){
-            Tree now = dead.poll();
-            map[now.x][now.y] += now.age/2;
-        }
-    }
-    private static void autumn(Queue<Tree> breeding){
+    private static void fall(){
+        for (int i=0;i<N;i++){
+            for (int j=0;j<N;j++){
+                int size = trees[i][j].size();
 
-        while (!breeding.isEmpty()){
-            Tree t = breeding.poll();
-            for (int i=0;i<8;i++){
-                int nextX = t.x + dir[i][0];
-                int nextY = t.y + dir[i][1];
-                if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= N) continue;
-                trees1.add(new Tree(nextX, nextY, 1));
+                for (int k=0;k<size;k++){
+                    int now = trees[i][j].poll();
+
+                    if (now % 5 == 0){
+
+                        for (int d=0;d<8;d++){
+                            int nextX = i + dir[d][0];
+                            int nextY = j + dir[d][1];
+                            if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= N) continue;
+                            trees[nextX][nextY].addFirst(1);
+                        }
+                    }
+                    trees[i][j].add(now);
+                }
             }
         }
     }
@@ -104,4 +101,5 @@ public class Main {
             }
         }
     }
+
 }
